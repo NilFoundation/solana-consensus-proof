557cd743ee4089d67770afce13427a71351d981b
Mikhail Komarov <nemo@nil.foundation>
2022-01-24 21:58:37 +0300

    More verification part updates.

--- share/verifier/contracts/cryptography/polynomial.sol
+++ share/verifier/contracts/cryptography/polynomial.sol
@@ -82,31 +82,31 @@ library PolynomialEval {
         }
 
         accumulator = bn254_crypto.invert(accumulator);
-//        assembly {
-//            let intermediate := mulmod(accumulator, mload(add(mPtr, 0xe0)), p)
-//            accumulator := mulmod(accumulator, mload(add(mPtr, 0x60)), p)
-//            mstore(add(mPtr, 0x60), intermediate)
-//
-//            intermediate := mulmod(accumulator, mload(add(mPtr, 0xc0)), p)
-//            accumulator := mulmod(accumulator, mload(add(mPtr, 0x40)), p)
-//            mstore(add(mPtr, 0x40), intermediate)
-//
-//            intermediate := mulmod(accumulator, mload(add(mPtr, 0xa0)), p)
-//            accumulator := mulmod(accumulator, mload(add(mPtr, 0x20)), p)
-//            mstore(add(mPtr, 0x20), intermediate)
-//
-//            intermediate := mulmod(accumulator, mload(add(mPtr, 0x80)), p)
-//            accumulator := mulmod(accumulator, mload(mPtr), p)
-//            mstore(mPtr, intermediate)
-//
-//            public_input_delta := mulmod(public_input_delta_numerator, mload(mPtr), p)
-//
-//            zero_polynomial_eval := mulmod(vanishing_denominator, mload(add(mPtr, 0x20)), p)
-//
-//            l_start := mulmod(lagrange_numerator, mload(add(mPtr, 0x40)), p)
-//
-//            l_end := mulmod(lagrange_numerator, mload(add(mPtr, 0x60)), p)
-//        }
+        assembly {
+            let intermediate := mulmod(accumulator, mload(add(mPtr, 0xe0)), p)
+            accumulator := mulmod(accumulator, mload(add(mPtr, 0x60)), p)
+            mstore(add(mPtr, 0x60), intermediate)
+
+            intermediate := mulmod(accumulator, mload(add(mPtr, 0xc0)), p)
+            accumulator := mulmod(accumulator, mload(add(mPtr, 0x40)), p)
+            mstore(add(mPtr, 0x40), intermediate)
+
+            intermediate := mulmod(accumulator, mload(add(mPtr, 0xa0)), p)
+            accumulator := mulmod(accumulator, mload(add(mPtr, 0x20)), p)
+            mstore(add(mPtr, 0x20), intermediate)
+
+            intermediate := mulmod(accumulator, mload(add(mPtr, 0x80)), p)
+            accumulator := mulmod(accumulator, mload(mPtr), p)
+            mstore(mPtr, intermediate)
+
+            public_input_delta := mulmod(public_input_delta_numerator, mload(mPtr), p)
+
+            zero_polynomial_eval := mulmod(vanishing_denominator, mload(add(mPtr, 0x20)), p)
+
+            l_start := mulmod(lagrange_numerator, mload(add(mPtr, 0x40)), p)
+
+            l_end := mulmod(lagrange_numerator, mload(add(mPtr, 0x60)), p)
+        }
     }
 
     function compute_public_input_delta(
@@ -516,20 +516,20 @@ library PolynomialEval {
             uint256 lend = lagrange_end;
             uint256 public_delta = public_input_delta;
             uint256 linearization_poly = proof.linearization_polynomial;
-//            assembly {
-//                let alpha_squared := mulmod(alpha, alpha, p)
-//                let alpha_cubed := mulmod(alpha, alpha_squared, p)
-//
-//                let t0 := mulmod(lstart, alpha_cubed, p)
-//                let t1 := mulmod(lend, alpha_squared, p)
-//                let t2 := addmod(grand_product, sub(p, public_delta), p)
-//                t1 := mulmod(t1, t2, p)
-//
-//                numerator_collector := addmod(numerator_collector, sub(p, t0), p)
-//                numerator_collector := addmod(numerator_collector, t1, p)
-//                numerator_collector := addmod(numerator_collector, linearization_poly, p)
-//                alpha_base := mulmod(alpha_base, alpha_cubed, p)
-//            }
+            assembly {
+                let alpha_squared := mulmod(alpha, alpha, p)
+                let alpha_cubed := mulmod(alpha, alpha_squared, p)
+
+                let t0 := mulmod(lstart, alpha_cubed, p)
+                let t1 := mulmod(lend, alpha_squared, p)
+                let t2 := addmod(grand_product, sub(p, public_delta), p)
+                t1 := mulmod(t1, t2, p)
+
+                numerator_collector := addmod(numerator_collector, sub(p, t0), p)
+                numerator_collector := addmod(numerator_collector, t1, p)
+                numerator_collector := addmod(numerator_collector, linearization_poly, p)
+                alpha_base := mulmod(alpha_base, alpha_cubed, p)
+            }
         }
 
         challenges.alpha_base = alpha_base;
