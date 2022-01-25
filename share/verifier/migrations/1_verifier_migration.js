cbb082bef31171bb9957288cdd5564e52b9e2aae
Mikhail Komarov <nemo@nil.foundation>
2022-01-25 05:13:58 +0300

    Submodules updated. Verification part updated.

--- share/verifier/migrations/1_verifier_migration.js
+++ share/verifier/migrations/1_verifier_migration.js
@@ -1,5 +1,18 @@
+// const polynomian_eval = artifacts.require("polynomial");
+const verification_keys = artifacts.require("verification_keys");
+// const bn254_crypto = artifacts.require("bn254_crypto");
+// const transcript = artifacts.require("transcript");
+// const types = artifacts.require("types");
+// const redshift_vk = artifacts.require("redshift_vk");
 const verifier = artifacts.require("verifier");
 
 module.exports = function (deployer) {
+  // deployer.deploy(polynomian_eval);
+  deployer.deploy(verification_keys);
+  // deployer.deploy(bn254_crypto);
+  // deployer.deploy(transcript);
+  // deployer.deploy(types);
+  // deployer.deploy(redshift_vk);
+  deployer.link(verification_keys, verifier);
   deployer.deploy(verifier);
 };
